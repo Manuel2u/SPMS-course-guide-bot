d829b3e986fb147e2af3a7bfa39e1f8147b9b0e3
Manuel2u <edodoo011@st.ug.edu.gh>
2022-11-24 08:50:29 +0000

    added keyboard buttoms and start command

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,35 @@ require("dotenv").config();
 import { Telegraf } from "telegraf";
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
+
+// start command with ReplyKeyboardMarkup telegram send message
+bot.start((ctx) => {
+  bot.telegram.sendMessage(
+    ctx.chat?.id || "",
+    "Welcome to the SPMS Bot course Registration Guide , Please select your Level",
+    {
+      reply_markup: {
+        keyboard: [
+          [
+            {
+              text: "Level 100",
+            },
+            {
+              text: "Level 200",
+            },
+          ],
+          [
+            {
+              text: "Level 300",
+            },
+            {
+              text: "Level 400",
+            },
+          ],
+        ],
+      },
+    }
+  );
+});
+
+bot.launch();
