6b67d33cf1f51b9478c5ffd15d08fabef4e1522a
Manuel2u <edodoo011@st.ug.edu.gh>
2022-11-27 20:12:52 +0000

    Added more options

--- src/options.ts
+++ src/options.ts
@@ -5,11 +5,55 @@ const options = {
     thirdYear: "Level 300",
     fourthYear: "Level 400",
   },
+
   courseOptionsL100: {
     physicalScience: "Physical Science",
     mathematicalScience: "Mathematical Science",
     earthScience: "Earth Science",
     informationTechnology: "Information Technology",
+  },
+
+  continuingStudentsSingleMajorOptions: {
+    option1: "Acturial Science",
+    option2: "Computer Science",
+    option3: "Mathematics",
+    option4: "Statistics",
+    option5: "Physics",
+    option6: "Chemistry",
+    option7: "Geology",
+    option8: "Geophysics",
+    option9: "Applied Geophysics",
+    option10: "Biomathematics",
+    option11: "Information Technology",
+    option12: "Applied Geology",
+  },
+
+  continuingStudentsCombinedMajorOptions: {
+    option1: "Chemistry and a Biological Science Program",
+    option2: "Chemistry and Physics",
+    option3: "Computer Science and Mathematics",
+    option4: "Computer Science and Physics",
+    option5: "Computer Science and  Statistics",
+    option6: "Mathematics and Physics",
+    option7: "Mathematics and Statistics",
+    option8: "Physics and Statistics",
+  },
+  continuingStudentsMajorMinorOptions: {
+    option1: "Computer Science with Mathematics",
+    option2: "Computer Science with Statistics",
+    option3: "Computer Science with Physics",
+    option4: "Geology with Physics",
+    option5: "Geology with Mathematics",
+    optijon6: "Mathematics with Physics",
+    option7: "Mathematics with Computer Science",
+    option8: "Mathematics with Statistics",
+    option9: "Marhematics with Geology",
+    option10: "Physics with Computer Science",
+    option11: "Physics with Geology",
+    option12: "Physics with Mathematics",
+    option13: "Statistics with Computer Science",
+    option14: "Statistics with Mathematics",
+    option15: "Physics with Statistics",
   },
 
   programOption: {
@@ -51,6 +95,7 @@ const options = {
       option4: "Physics and Chemistry",
       option5: "Chemistry and Something",
     },
+
     combinedMajor: {
       option1: "Physics and Mathematics",
       option2: "Chemistry and Biological Science Course",
